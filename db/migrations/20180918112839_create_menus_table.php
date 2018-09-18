1730b4f9183472e0de1411223bbdb25db967d86d
tdorkaa <tamasdorkaa@gmail.com>
2018-09-18 13:40:48 +0200

    Restaurants and Menus table migrations created

---
+++ db/migrations/20180918112839_create_menus_table.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateMenusTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('menus');
+        $table
+            ->addColumn('restaurant_id', 'integer')
+            ->addColumn('menu', 'text')
+            ->addColumn('date', 'date')
+            ->addIndex(['restaurant_id'])
+            ->addIndex(['date'])
+            ->create();
+    }
+}
