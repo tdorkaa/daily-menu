cc7a64ca6325a858f0a10c2a507cd7c160efce47
tdorkaa <tamasdorkaa@gmail.com>
2018-09-21 16:22:57 +0200

    modify menusConverter to work in case of multiple restaurants

--- src/MenusConverter.php
+++ src/MenusConverter.php
@@ -7,13 +7,8 @@ class MenusConverter
 
     public function convert(array $menuDao)
     {
-        $convertedDao = [
-            [
-                'restaurant_id' => $menuDao[0]['id'],
-                'restaurant' => $menuDao[0]['restaurant'],
-                'menus' => $this->makeListOfDateAndMenu($menuDao)
-            ]
-        ];
+        $firstConvention = $this->makeArraysByRestaurantId($menuDao);
+        $convertedDao = $this->convertMenus($firstConvention);
         return $convertedDao;
     }
 
@@ -23,6 +18,32 @@ class MenusConverter
         foreach ($menuDao as $index => $menu) {
             $menus[$index]['date'] = $menu['date'];
             $menus[$index]['menu'] = $menu['menu'];
+        }
+        return $menus;
+    }
+
+    private function makeArraysByRestaurantId($menuDao)
+    {
+        $menus = [];
+        $indexOfOuterArray = 0;
+        foreach ($menuDao as $menu) {
+            if ($indexOfOuterArray !== $menu['restaurant_id'] - 1) {
+                $indexOfOuterArray++;
+                $menus[$indexOfOuterArray][] = $menu;
+            } else {
+                $menus[$indexOfOuterArray][] = $menu;
+            }
+        }
+        return $menus;
+    }
+
+    private function convertMenus($menuDao)
+    {
+        $menus = [];
+        foreach ($menuDao as $index => $menu) {
+            $menus[$index]['restaurant_id'] = $menu[0]['restaurant_id'];
+            $menus[$index]['restaurant'] = $menu[0]['restaurant'];
+            $menus[$index]['menus'] = $this->makeListOfDateAndMenu($menu);
         }
         return $menus;
     }
--- tests/MenusConverterTest.php
+++ tests/MenusConverterTest.php
@@ -7,12 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class MenusConverterTest extends TestCase
 {
+
+    /**
+     * @var MenusConverter
+     */
+    private $converter;
+
+    protected function setUp()
+    {
+        $this->converter = new MenusConverter();    }
+
     /**
      * @test
      */
     public function convert_GivenMenusDao_ReturnsConvertedArray()
     {
-        $converter = new MenusConverter();
         $expected = [
             [
                 'restaurant_id' => '1',
@@ -56,7 +65,98 @@ class MenusConverterTest extends TestCase
                 'date' => '2018-09-11'
             ]
         ];
-        $actual = $converter->convert($menuDao);
+        $actual = $this->converter->convert($menuDao);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function convert_GivenMenusDaoWithMultipleRestaurants_ReturnsConvertedArray()
+    {
+        $expected = [
+            [
+                'restaurant_id' => '1',
+                'restaurant' => 'Fiction Stars1',
+                'menus' => [
+                    [
+                        'date' => '2018-09-22',
+                        'menu' => 'Leves, Fozelek1'
+                    ],
+                    [
+                        'date' => '2018-09-12',
+                        'menu' => 'Leves, Fozelek3'
+                    ],
+                    [
+                        'date' => '2018-09-11',
+                        'menu' => 'Leves, Fozelek2'
+                    ]
+                ]
+            ],
+            [
+                'restaurant_id' => '2',
+                'restaurant' => 'Fiction Stars2',
+                'menus' => [
+                    [
+                        'date' => '2018-09-22',
+                        'menu' => 'Leves, Fozelek4'
+                    ],
+                    [
+                        'date' => '2018-09-12',
+                        'menu' => 'Leves, Fozelek5'
+                    ],
+                    [
+                        'date' => '2018-09-11',
+                        'menu' => 'Leves, Fozelek6'
+                    ]
+                ]
+            ]
+        ];
+        $menuDao = [
+            [
+                'id' => '1',
+                'restaurant_id' => '1',
+                'restaurant' => 'Fiction Stars1',
+                'menu' => 'Leves, Fozelek1',
+                'date' => '2018-09-22'
+            ],
+            [
+                'id' => '3',
+                'restaurant_id' => '1',
+                'restaurant' => 'Fiction Stars1',
+                'menu' => 'Leves, Fozelek3',
+                'date' => '2018-09-12'
+            ],
+            [
+                'id' => '2',
+                'restaurant_id' => '1',
+                'restaurant' => 'Fiction Stars1',
+                'menu' => 'Leves, Fozelek2',
+                'date' => '2018-09-11'
+            ],
+            [
+                'id' => '4',
+                'restaurant_id' => '2',
+                'restaurant' => 'Fiction Stars2',
+                'menu' => 'Leves, Fozelek4',
+                'date' => '2018-09-22'
+            ],
+            [
+                'id' => '5',
+                'restaurant_id' => '2',
+                'restaurant' => 'Fiction Stars2',
+                'menu' => 'Leves, Fozelek5',
+                'date' => '2018-09-12'
+            ],
+            [
+                'id' => '6',
+                'restaurant_id' => '2',
+                'restaurant' => 'Fiction Stars2',
+                'menu' => 'Leves, Fozelek6',
+                'date' => '2018-09-11'
+            ]
+        ];
+        $actual = $this->converter->convert($menuDao);
         $this->assertEquals($expected, $actual);
     }
 }
