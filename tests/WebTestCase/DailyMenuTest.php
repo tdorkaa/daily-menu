f709537d57817e3a3a47f59415a2d8dc6e487a82
tdorkaa <tamasdorkaa@gmail.com>
2018-09-24 13:13:54 +0200

    getMenusByDate for some restaurants

--- tests/WebTestCase/DailyMenuTest.php
+++ tests/WebTestCase/DailyMenuTest.php
@@ -44,4 +44,20 @@ class DailyMenuTest extends TestCase
         $this->assertContains('Leves, Fozelek1', (string)$response->getBody());
         $this->assertContains('Leves, Fozelek2', (string)$response->getBody());
     }
+
+    /**
+     * @test
+     */
+    public function getMenusByDate_GivenDbContainsTwoDifferentRestaurants_ReturnsDailyMenus()
+    {
+        $this->insertRestaurants([$this->aRestaurant(), $this->aRestaurant(2)]);
+        $this->insertMenus([$this->aMenu(1,1), $this->aMenu(2,1, '2018-09-23'),
+            $this->aMenu(3,2, '2018-09-21')]);
+        $response = $this->processRequest('GET', '/menus?date1=2018-09-20&date2=2018-09-23');
+        $this->assertContains('Fiction Stars1', (string)$response->getBody());
+        $this->assertContains('Fiction Stars2', (string)$response->getBody());
+        $this->assertContains('Leves, Fozelek1', (string)$response->getBody());
+        $this->assertContains('Leves, Fozelek2', (string)$response->getBody());
+        $this->assertContains('Leves, Fozelek3', (string)$response->getBody());
+    }
 }
