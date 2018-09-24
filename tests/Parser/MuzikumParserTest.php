b6e15753e0a5dbd660617f10200650aa803a1e01
tdorkaa <tamasdorkaa@gmail.com>
2018-09-24 16:55:51 +0200

    refactor test

--- tests/Parser/MuzikumParserTest.php
+++ tests/Parser/MuzikumParserTest.php
@@ -10,24 +10,30 @@ use Tests\MockHtmlParser;
 class MuzikumParserTest extends TestCase
 {
     use MockHtmlParser;
+    
+    private $mockParser;
+    private $muzikumParser;
+    
+    protected function setUp()
+    {
+        $this->mockParser = $this->getMockHtmlParser();
+        $this->mockParser
+            ->expects($this->once())
+            ->method('load')
+            ->with('http://muzikum.hu/heti-menu/', ['preserveLineBreaks' => true]);
+
+        $this->mockParser->loadStr(
+            file_get_contents(__DIR__ . '/HtmlContent/Muzikum.html'),
+            ['preserveLineBreaks' => true]
+        );
+        $this->muzikumParser = new MuzikumParser($this->mockParser);    }
 
     /**
      * @test
      */
     public function getDailyMenu_GivenLoadedSourceCode_ReturnsDailyMenu()
     {
-        $mockParser = $this->getMockHtmlParser();
-        $mockParser
-            ->expects($this->once())
-            ->method('load')
-            ->with('http://muzikum.hu/heti-menu/', ['preserveLineBreaks' => true]);
-
-        $mockParser->loadStr(
-            file_get_contents(__DIR__ . '/HtmlContent/Muzikum.html'),
-            ['preserveLineBreaks' => true]
-        );
-        $muzikumParser = new MuzikumParser($mockParser);
-        $dailyMenu = $muzikumParser->getDailyMenu(new ParserHelper(), '2018-09-24');
+        $dailyMenu = $this->muzikumParser->getDailyMenu(new ParserHelper(), '2018-09-24');
         $this->assertEquals(['Francia hagymaleves diós veknivel',
             'Csirkemell sajttal, sonkával sütve, petrezselymes burgonyával'], $dailyMenu);
     }
@@ -37,18 +43,7 @@ class MuzikumParserTest extends TestCase
      */
     public function getDailyMenu_GivenDateIsTuesDay_ReturnsDailyMenu()
     {
-        $mockParser = $this->getMockHtmlParser();
-        $mockParser
-            ->expects($this->once())
-            ->method('load')
-            ->with('http://muzikum.hu/heti-menu/', ['preserveLineBreaks' => true]);
-
-        $mockParser->loadStr(
-            file_get_contents(__DIR__ . '/HtmlContent/Muzikum.html'),
-            ['preserveLineBreaks' => true]
-        );
-        $muzikumParser = new MuzikumParser($mockParser);
-        $dailyMenu = $muzikumParser->getDailyMenu(new ParserHelper(), '2018-09-25');
+        $dailyMenu = $this->muzikumParser->getDailyMenu(new ParserHelper(), '2018-09-25');
         $this->assertEquals(['Burgonyás kelleves kéksajtos tejföllel',
             'Háromborsos sertésborda tejfölös jalapeno-val, jázmin rizzsel'], $dailyMenu);
     }
